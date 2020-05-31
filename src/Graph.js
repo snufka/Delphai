a37fa29d3e0d8239916feb61fe0b1bf172fb043f
snufka <52801250+snufka@users.noreply.github.com>
2020-05-31 23:27:34 +0200

    addig bubble chart

--- src/Graph.js
+++ src/Graph.js
@@ -1,10 +1,39 @@
 import React from 'react'
-import Chart from 'chart.js'
+import { Bubble } from 'react-chartjs-2';
+
+const data = {
+    labels: ['January'],
+    datasets: [
+        {
+            label: 'Beauty Indestry',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [{ x: 10, y: 20, r: 50 }]
+        }
+
+    ]
+};
 
 export default function Graph() {
     return (
         <div>
-            <h1>Place holder for a Prety Bubble Chart</h1>
+            <h2>Bubble Example</h2>
+            <Bubble data={data} />
         </div>
     )
 }
